5b10b220b2b92b08c14a6d72d1ba9932790f7f5f
Eric Miller <eric@legoaces.org>
2017-05-12 23:08:31 -0400

    Added benchmark at 1.1s for entire Euler problem

---
+++ soduku/main_test.go
@@ -0,0 +1,14 @@
+// main_bench
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkEuler(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < len(eulerGrids); i++ {
+			solveGrid(eulerGrids[i])
+		}
+	}
+}
